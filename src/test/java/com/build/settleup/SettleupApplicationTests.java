71abad9cdc96d03aebd5c33ed7c2b802fbc67ed4
Sarthak Shrivastava <161567581+SarthakShrivastav-a@users.noreply.github.com>
2025-02-15 22:57:07 +0530

    JWT Authentication Setup

---
+++ src/test/java/com/build/settleup/SettleupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.build.settleup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettleupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
